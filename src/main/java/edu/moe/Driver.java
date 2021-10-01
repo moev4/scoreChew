c14cd566b6a4161c61d29810fb15d153220c92fc
Moses Vaughan <moevaughan@MoeMacPro.fios-router.home>
2021-10-01 16:59:59 -0400

    Initial commit

---
+++ src/main/java/edu/moe/Driver.java
@@ -0,0 +1,13 @@
+package edu.moe;
+
+/**
+ * Hello world!
+ *
+ */
+public class Driver
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/edu/moe/DriverTest.java
@@ -0,0 +1,20 @@
+package edu.moe;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class DriverTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
